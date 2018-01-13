e1ea75dd2731db8df63a5f71acfcfea191e9d124
Eugene Sadovoi <evgeni@eniks.com>
2018-01-13 01:18:55 -0500

    Initial add

---
+++ src/LoggingExtension.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Reflection;
+using Unity.Attributes;
+using Unity.Builder;
+using Unity.Extension;
+using Unity.Policy;
+
+namespace Unity.Microsoft.Logging
+{
+    public class LoggingExtension : UnityContainerExtension, IBuildPlanCreatorPolicy
+    {
+        #region Fields
+
+        private readonly MethodInfo _createLoggerMethod;
+
+        #endregion
+
+
+        #region Constructors
+
+        [InjectionConstructor]
+        public LoggingExtension()
+            : this(new LoggerFactory())
+        {
+        }
+
+        public LoggingExtension(ILoggerFactory loggerFactory)
+        {
+            LoggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+            _createLoggerMethod = LoggerFactory.GetType().GetTypeInfo().GetDeclaredMethods(nameof(ILoggerFactory.CreateLogger))
+                                               .First(m => m.IsGenericMethod);
+        }
+
+
+        #endregion
+
+
+        #region Public Members
+
+        public ILoggerFactory LoggerFactory { get; } = new LoggerFactory();
+
+        #endregion
+
+
+        #region Implementation
+
+        protected override void Initialize()
+        {
+            Context.Policies.Set<IBuildPlanCreatorPolicy>(this, typeof(ILogger<>));
+        }
+
+        IBuildPlanPolicy IBuildPlanCreatorPolicy.CreatePlan(IBuilderContext context, INamedType buildKey)
+        {
+            var itemType = (context ?? throw new ArgumentNullException(nameof(context))).BuildKey
+                                                                                        .Type
+                                                                                        .GetTypeInfo()
+                                                                                        .GenericTypeArguments
+                                                                                        .First();
+            var buildMethod = _createLoggerMethod.MakeGenericMethod(itemType)
+                                                 .CreateDelegate(typeof(DynamicBuildPlanMethod));
+
+            return new DynamicMethodBuildPlan((DynamicBuildPlanMethod)buildMethod);
+        }
+
+        #endregion
+
+
+        #region Nested Types
+
+        public delegate void DynamicBuildPlanMethod(IBuilderContext context);
+
+        private class DynamicMethodBuildPlan : IBuildPlanPolicy
+        {
+            private readonly DynamicBuildPlanMethod _buildMethod;
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="buildMethod"></param>
+            public DynamicMethodBuildPlan(DynamicBuildPlanMethod buildMethod)
+            {
+                _buildMethod = buildMethod;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="context"></param>
+            public void BuildUp(IBuilderContext context)
+            {
+                _buildMethod(context);
+            }
+        }
+        
+        #endregion
+    }
+}
---
+++ tests/LoggingFixture.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using Unity.Microsoft.Logging;
+
+namespace Microsoft.Logging.Tests
+{
+    [TestClass]
+    public class LoggingFixture
+    {
+        private static IUnityContainer _container;
+        private LoggedType _instance;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+        }
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _container = new UnityContainer();
+            _container.AddNewExtension<LoggingExtension>();
+        }
+
+        [TestMethod]
+        public void microsoft_logging_default_name()
+        {
+            Assert.IsNotNull(_container.Configure<LoggingExtension>().LoggerFactory);
+
+            var ss = _container.Configure<LoggingExtension>().LoggerFactory.CreateLogger<LoggedType>();
+            Assert.IsInstanceOfType(_container.Configure<LoggingExtension>().LoggerFactory,
+                                    typeof(ILoggerFactory));
+        }
+
+        [TestMethod]
+        public void microsoft_logging_can_resolve_test_type()
+        {
+            _instance = _container.Resolve<LoggedType>();
+            Assert.IsNotNull(_instance);
+            Assert.IsNotNull(_instance.ResolvedLogger);
+        }
+
+
+        [TestMethod]
+        public void microsoft_logging_correct_type()
+        {
+        }
+
+        [TestMethod]
+        public void microsoft_logging_change_name()
+        {
+        }
+
+
+        public class LoggedType
+        {
+            public LoggedType(ILogger<LoggedType> log)
+            {
+                ResolvedLogger = log;
+            }
+
+            public ILogger<LoggedType> ResolvedLogger { get; }
+        }
+    }
+}
